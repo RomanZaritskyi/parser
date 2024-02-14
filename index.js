94bc2a39dfbf5840886e1bd82fc859b28159f39a
RomanZaritskyi <romanzaritskyi@gmail.com>
2024-02-14 18:19:42 +0200

    commit2

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
+const express = require('express');
+const { scrapeLogic } = require('./scrapeLogic');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+app.get('/scrape', (req, res) => {
+	scrapeLogic(res);
 });
 
-app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+app.get('/', (req, res) => {
+	res.send('Render Puppeteer server is up and running!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+	console.log(`Listening on port ${PORT}`);
 });

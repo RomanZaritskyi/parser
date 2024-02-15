12a3bec62ca6b45b2cc8cd15576d484348664e3c
RomanZaritskyi <romanzaritskyi@gmail.com>
2024-02-15 14:24:28 +0200

    asd

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -27,52 +27,52 @@ const scrapeLogic = async (res) => {
 
 		// ----------- Set screen size
 		await page.setViewport({ width: 1080, height: 1024 });
-		setTimeout(async () => {
-			// -----------Search input -----------------------------
-			const searchInputSelector = 'input.eb46370fe1';
-			await page.type(searchInputSelector, 'Lviv');
 
-			// -----------Select dates ---------------------------------------
-			const dateContainerSelector = 'div.f73e6603bf';
-			await page.waitForSelector(dateContainerSelector, { timeout: 60000 });
-			await page.click(dateContainerSelector);
+		// -----------Search input -----------------------------
+		const searchInputSelector = 'input.eb46370fe1';
+		await page.type(searchInputSelector, 'Lviv');
 
-			// -----------Checkin date--------------------------------
-			const dateSelector = 'span[data-date="2024-02-16"]';
-			await page.waitForSelector(dateSelector, { timeout: 60000 });
-			await page.click(dateSelector);
+		// ---------------Search btn------------------------------------------
+		const submitButtoSelector =
+			'button.a83ed08757.c21c56c305.a4c1805887.f671049264.d2529514af.c082d89982.cceeb8986b';
+		await page.waitForSelector(submitButtoSelector);
+		await page.click(submitButtoSelector);
 
-			// ----------Checkout date------------------
-			const dateSelector2 = 'span[data-date="2024-02-20"]';
-			await page.waitForSelector(dateSelector2, { timeout: 60000 });
-			await page.click(dateSelector2);
+		// --------------Wait for navigation -------------------
+		await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
-			// ---------------Search btn------------------------------------------
-			// const submitButtoSelector =
-			// 	'button.a83ed08757.c21c56c305.a4c1805887.f671049264.d2529514af.c082d89982.cceeb8986b';
-			// await page
-			// 	.waitForSelector(submitButtoSelector)
-			// 	.then(() => page.click(submitButtoSelector));
-			// await page.click(submitButtoSelector);
+		// -----------Select dates ---------------------------------------
+		const dateContainerSelector = 'div.f73e6603bf';
+		await page.waitForSelector(dateContainerSelector, { timeout: 60000 });
+		await page.click(dateContainerSelector);
 
-			// --------------Wait for navigation -------------------
-			// await page.waitForNavigation({ waitUntil: 'networkidle2' });
+		// -----------Checkin date--------------------------------
+		const dateSelector = 'span[data-date="2024-02-16"]';
+		await page.waitForSelector(dateSelector, { timeout: 60000 });
+		await page.click(dateSelector);
 
-			// --------------Get places-----------------------------------------------
-			// const palcesSelector = await page.waitForSelector('div.ac864a506a');
-			// const amounOfPlaces = await palcesSelector?.evaluate(
-			// 	(el) => el.textContent
-			// );
+		// ----------Checkout date------------------
+		const dateSelector2 = 'span[data-date="2024-02-20"]';
+		await page.waitForSelector(dateSelector2, { timeout: 60000 });
+		await page.click(dateSelector2);
 
-			// test HTML output ---------------------------------------------
-			const pageSource = await page.content();
-			// fs.writeFileSync('pageSource.html', pageSource, 'utf8');
-			// console.log('Page source saved to pageSource.html');
+		// ---------------Search btn------------------------------------------
+		await page.waitForSelector(submitButtoSelector);
+		await page.click(submitButtoSelector);
+		// --------------Get places-----------------------------------------------
+		// const palcesSelector = await page.waitForSelector('div.ac864a506a');
+		// const amounOfPlaces = await palcesSelector?.evaluate(
+		// 	(el) => el.textContent
+		// );
 
-			await browser.close();
+		// test HTML output ---------------------------------------------
+		const pageSource = await page.content();
+		// fs.writeFileSync('pageSource.html', pageSource, 'utf8');
+		// console.log('Page source saved to pageSource.html');
 
-			res.send(pageSource);
-		}, 2000);
+		await browser.close();
+
+		res.send(pageSource);
 	} catch (e) {
 		console.error(e);
 		res.send(`Something went wrong while running Puppeteer: ${e}`);

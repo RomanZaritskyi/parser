d3755e54a991d3575f82ca2cecbb0c94b2153659
RomanZaritskyi <romanzaritskyi@gmail.com>
2024-02-15 14:51:46 +0200

    qweqwe

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -32,31 +32,24 @@ const scrapeLogic = async (res) => {
 		const searchInputSelector = 'input.eb46370fe1';
 		await page.type(searchInputSelector, 'Lviv');
 
-		// ---------------Search btn------------------------------------------
-		const submitButtoSelector =
-			'button.a83ed08757.c21c56c305.a4c1805887.f671049264.d2529514af.c082d89982.cceeb8986b';
-		await page.waitForSelector(submitButtoSelector);
-		await page.click(submitButtoSelector);
-
-		// --------------Wait for navigation -------------------
-		await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
 		// -----------Select dates ---------------------------------------
 		const dateContainerSelector = 'div.f73e6603bf';
 		await page.waitForSelector(dateContainerSelector, { timeout: 60000 });
 		await page.click(dateContainerSelector);
 
 		// -----------Checkin date--------------------------------
-		const dateSelector = 'span[data-date="2024-02-16"]';
+		const dateSelector = 'span[data-date="2024-02-15"]';
 		await page.waitForSelector(dateSelector, { timeout: 60000 });
 		await page.click(dateSelector);
 
 		// ----------Checkout date------------------
-		const dateSelector2 = 'span[data-date="2024-02-20"]';
+		const dateSelector2 = 'span[data-date="2024-02-29"]';
 		await page.waitForSelector(dateSelector2, { timeout: 60000 });
 		await page.click(dateSelector2);
 
 		// ---------------Search btn------------------------------------------
+		const submitButtoSelector =
+			'button.a83ed08757.c21c56c305.a4c1805887.f671049264.d2529514af.c082d89982.cceeb8986b';
 		await page.waitForSelector(submitButtoSelector);
 		await page.click(submitButtoSelector);
 

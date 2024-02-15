54fa817b942e99c0c74e6ba62b6e0f8fb008e225
RomanZaritskyi <romanzaritskyi@gmail.com>
2024-02-15 14:01:59 +0200

    qwe

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -48,13 +48,15 @@ const scrapeLogic = async (res) => {
 		await page.click(dateSelector2);
 
 		// ---------------Search btn------------------------------------------
-		// const submitButtoSelector =
-		// 	'button.a83ed08757.c21c56c305.a4c1805887.f671049264.d2529514af.c082d89982.cceeb8986b';
-		// await page.waitForSelector(submitButtoSelector);
+		const submitButtoSelector =
+			'button.a83ed08757.c21c56c305.a4c1805887.f671049264.d2529514af.c082d89982.cceeb8986b';
+		await page
+			.waitForSelector(submitButtoSelector)
+			.then(() => page.click(submitButtoSelector));
 		// await page.click(submitButtoSelector);
 
 		// --------------Wait for navigation -------------------
-		// await page.waitForNavigation({ waitUntil: 'networkidle2' });
+		await page.waitForNavigation({ waitUntil: 'networkidle2' });
 
 		// --------------Get places-----------------------------------------------
 		// const palcesSelector = await page.waitForSelector('div.ac864a506a');

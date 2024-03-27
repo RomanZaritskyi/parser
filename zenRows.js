56cb6e7d8328841cbe38bd9a62298793be78a17e
r.zaritskyi <r.zaritskyi@steel.mil.ua>
2024-03-27 17:14:54 +0200

    add new api key

--- zenRows.js
+++ zenRows.js
@@ -1,16 +1,16 @@
-const { ZenRows } = require('zenrows');
-const cheerio = require('cheerio');
+const { ZenRows } = require("zenrows");
+const cheerio = require("cheerio");
 
 async function getInfo(place) {
-	const client = new ZenRows('607e809eb2f96d02b07e045f8203cb5862e256ed');
+	const client = new ZenRows("676de12682645132912e89f696677e3ab394343e");
 	const url = `https://www.booking.com/searchresults.html?ss=${place.city}&checkin=${place.startDate}&checkout=${place.endDate}`;
 
 	try {
 		const { data } = await client.get(url, {
-			premium_proxy: 'true',
+			premium_proxy: "true",
 		});
 		const $ = cheerio.load(data);
-		const info = $('h1.f6431b446c.d5f78961c3').text();
+		const info = $("h1.f6431b446c.d5f78961c3").text();
 
 		return info;
 	} catch (error) {
